d2b38a2dbb4715c9a3fec389839fa570a1048ed9
Luke Poeppel <luke.poeppel@gmail.com>
2020-07-03 11:47:22 -0400

    Decitala now inherits from GeneralFragment

--- decitala_v2.py
+++ decitala_v2.py
@@ -340,9 +340,13 @@ print(l)
 '''
 
 ########################################################################
-
-class Decitala(object):
-	'''    
+'''
+Inheritance notes:
+- if you add the init function to the child class, the child class does not inherit 
+the parent classes init function. 
+'''
+class Decitala(GeneralFragment):
+	"""
 	Class that stores Decitala data. Reads from a folder containing all Decitala XML files.
 	Inherits from GeneralFragment. 
 
@@ -392,8 +396,8 @@ class Decitala(object):
 	
 	>>> Decitala.get_by_id(89)
 	<decitala.Decitala 89_Lalitapriya>
-	''' 
-	def __init__(self, name = None):
+	"""
+	def __init__(self, name, **kwargs):
 		if name:
 			if name.endswith('.xml'):
 				searchName = name
@@ -405,22 +409,22 @@ class Decitala(object):
 			for thisFile in os.listdir(decitala_path):
 				x = re.search(searchName, thisFile)
 				if bool(x) == True:
+					self.full_path = decitala_path + '/' + thisFile
 					self.name = os.path.splitext(thisFile)[0]
 					self.filename = thisFile
 					self.stream = converter.parse(decitala_path + '/' + thisFile)
-	  
+
+		super().__init__(path=self.full_path, name = self.name)
+	
 	def __repr__(self):
 		return '<decitala.Decitala {}>'.format(self.name)
-
-	def __hash__(self):
-		return hash(self.name)
-
+	
 	@property
 	def id_num(self):
 		if self.name:
 			idValue = re.search(r'\d+', self.name)
 			return int(idValue.group(0))
-
+	
 	@classmethod
 	def get_by_id(cls, input_id):
 		'''
@@ -436,170 +440,23 @@ class Decitala(object):
 		assert type(input_id) == int
 		if input_id > 120 or input_id < 1:
 			raise Exception('Input must be between 1 and 120!')
+		
+		if input_id in subdecitala_array:
+			raise Exception('There are multiple talas with this ID. Please consult the Lavignac.')
 
 		for thisFile in os.listdir(decitala_path):
 			x = re.search(r'\d+', thisFile)
 			try:
 				if int(x.group(0)) == input_id:
-					return Decitala(thisFile)
+					return Decitala(name=thisFile)
 			except AttributeError:
 				pass
 	
 	@property
-	def num_onsets(self):
-		count = 0
-		for _ in self.stream.flat.getElementsByClass(note.Note):
-			count += 1
-		return count
-	  
-	def ql_array(self, retrograde=False):
-		'''
-		INPUTS
-		*-*-*-*-*-*-*-*-
-		retrograde : type = ``bool``
-		'''
-		if not(retrograde):
-			return np.array([this_note.quarterLength for this_note in self.stream.flat.getElementsByClass(note.Note)])
-		else:
-			return np.flip(np.array([this_note.quarterLength for this_note in self.stream.flat.getElementsByClass(note.Note)]))
-
-	@property
-	def carnatic_string(self):
-		return ql_array_to_carnatic_string(self.ql_array())
-
-	@property
-	def ql_duration(self):
-		return sum(self.ql_array())
-
-	@property
 	def numMatras(self):
 		return (self.ql_duration / 0.5)
 
-	def dseg(self, as_str=False):
-		'''
-		Marvin's d-seg as introducted in "The perception of rhythm in non-tonal music" (1991). Maps a fragment
-		into a sequence of relative durations. This allows cross comparison of rhythmic fragments beyond 
-		exact augmentation; we may, for instance, filter rhythms by similar the familiar dseg <1 0 0> which 
-		corresponds to long-short-short (e.g. dactyl). 
-
-		INPUTS
-		*-*-*-*-*-*-*-*-
-		as_str : type = ``bool``
-
-		>>> raya = Decitala('Rayavankola')
-		>>> raya.ql_array()
-		array([1.  , 0.5 , 1.  , 0.25, 0.25])
-		>>> raya.dseg(as_str = False)
-		array([2, 1, 2, 0, 0])
-		>>> raya.dseg(as_str = True)
-		'<2 1 2 0 0>'
-		'''
-		dseg_vals = copy.copy(self.ql_array())
-		valueDict = dict()
-
-		for i, thisVal in zip(range(0, len(sorted(set(dseg_vals)))), sorted(set(dseg_vals))):
-			valueDict[thisVal] = str(i)
-
-		for i, thisValue in enumerate(dseg_vals):
-			for thisKey in valueDict:
-				if thisValue == thisKey:
-					dseg_vals[i] = valueDict[thisKey]
-
-		if as_str == True:
-			return '<' + ' '.join([str(int(val)) for val in dseg_vals]) + '>'
-		else:
-			return np.array([int(val) for val in dseg_vals])
-
-	def reduced_dseg(self, as_str=False):
-		'''
-		Technique used in this paper. Takes a dseg and returns a new dseg where contiguous values are removed. 
-
-		INPUTS
-		*-*-*-*-*-*-*-*-
-		as_str : type = ``bool``
-		'''
-		def _remove_adjacent_equal_elements(array):
-			as_lst = list(array)
-			filtered = [a for a, b in zip(as_lst, as_lst[1:] + [not as_lst[-1]]) if a != b]
-			return np.array(filtered)
-
-		orig = self.dseg(as_str = False)
-		as_array = _remove_adjacent_equal_elements(array = orig)
-
-		if not(as_str):
-			return np.array([int(val) for val in as_array])
-		else:
-			return '<' + ' '.join([str(int(val)) for val in as_array]) + '>'
-
-	def successive_ratio_list(self):
-		'''
-		Returns an array of the successive duration ratios. By convention, we set the first value to 1.0. 
-		'''
-		ratio_array = [1.0] #np.array([1.0])
-		i = 0
-		while i < len(self.ql_array()) - 1:
-			ratio_array.append(_ratio(self.ql_array(), i))
-			#np.concatenate(ratio_array, _ratio(self.ql_array(), i))
-			i += 1
-
-		return np.array(ratio_array)
-
-	def get_cyclic_permutations(self):
-		'''
-		Returns all cyclic permutations. 
-		'''
-		return np.array([np.roll(self.ql_array(), -i) for i in range(self.num_onsets)])
-
-	################ ANALYSIS ################
-	def is_non_retrogradable(self):
-		return self.ql_array(retrograde = False) == self.ql_array(retrograde = True)
-
-	def morris_symmetry_class(self):
-		'''
-		Robert Morris (year?) notes 7 kinds of interesting rhythmic symmetries. I provided the names.
-
-		I.) Maximally Trivial:				of the form X (one onset, one anga class)
-		II.) Trivial Symmetry: 				of the form XXXXXX (multiple onsets, same anga class)
-		III.) Trivial Dual Symmetry:  		of the form XY (two onsets, two anga classes)
-		IV.) Maximally Trivial Palindrome: 	of the form XXX...XYX...XXX (multiple onsets, two anga classes)
-		V.) Trivial Dual Palindromic:		of the form XXX...XYYYX...XXX (multiple onsets, two anga classes)
-		VI.) Palindromic: 					of the form XY...Z...YX (multiple onsets, n/2 anga classes)
-		VII.) Stream:						of the form XYZ...abc... (n onsets, n anga classes)
-		'''
-		dseg = self.dseg(as_str = False)
-		reduced_dseg = self.reduced_dseg(as_str = False)
-
-		if len(dseg) == 1:
-			return 'I. Maximally Trivial'
-		elif len(dseg) > 1 and len(np.unique(dseg)) == 1:
-			return 'II. Trivial Symmetry'
-		elif len(dseg) == 2 and len(np.unique(dseg)) == 2:
-			return 'III. Trivial Dual Symmetry'
-		elif len(dseg) > 2 and len(np.unique(dseg)) == 2:
-			return 'IV. Maximally Trivial Palindrome'
-		elif len(dseg) > 2 and len(reduced_dseg) == 3:
-			return 'V. Trivial Dual Palindrome'
-		elif len(dseg) > 2 and len(np.unique(dseg)) == len(dseg) // 2:
-			return 'VI. Palindrome'
-		else:
-			return 'VII. Stream'
-
-	def std(self):
-		return round(np.std(self.ql_array()), 5)
-
-	def c_score(self):
-		'''
-		Povel and Essens (1985) C-Score. Returns the average across all clocks. 
-		Doesn't seem to work...
-		'''
-		return get_average_c_score(array = self.ql_array())
-
-	def show(self):
-		if self.stream:
-			return self.stream.show()
-
-d = Decitala('Gajajhampa')
-#print(d.c_score())
+########################################################################
 
 class NaryTree(object):
 	"""
