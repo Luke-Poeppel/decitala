d6c94fde8537a5c98676c80744f90338f5fd6210
Luke Poeppel <luke.poeppel@gmail.com>
2021-03-14 11:05:02 -0400

    Updated test_hash_table values.

--- tests/test_hash_table.py
+++ tests/test_hash_table.py
@@ -15,8 +15,8 @@ greek_path = os.path.dirname(here) + "/fragments/Greek_Metrics/XML"
 def DHT():
 	return DecitalaHashTable()
 
-factors=[0.5, 0.75, 1.0, 1.25, 1.5, 2.0, 3.0, 4.0]
-differences=[0.0, 0.25, 0.5, 0.75]
+factors = [0.125, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0, 3.0, 4.0]
+differences = [-0.375, -0.25, -0.125, 0.0, 0.125, 0.25, 0.375, 0.5, 0.75, 0.875, 1.75, 2.625, 3.5, 4.375]
 
 normal = lambda x: x
 retrograde = lambda x: x[::-1]
@@ -57,6 +57,10 @@ def test_hash_table(DHT):
 	for fragment in decitala_rows:
 		ql_array = json.loads(fragment[1])
 		random_modification = random.choice(funcs)(ql_array)
+		
+		if any(x <= 0 for x in random_modification):
+			continue
+		
 		random_modification = str(tuple(random_modification))
 		assert DHT[random_modification] is not None
 
