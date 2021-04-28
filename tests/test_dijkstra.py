bf63060bf01ba29635eb62051b78b737fede2225
Luke Poeppel <luke.poeppel@gmail.com>
2021-04-27 20:19:33 -0400

    Small fix.

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -7,22 +7,22 @@ from decitala.path_finding import dijkstra
 here = os.path.abspath(os.path.dirname(__file__))
 filepath = os.path.dirname(here) + "/tests/static/Shuffled_Transcription_1.xml"
 
-fragments = rolling_hash_search(
-	filepath,
-	0,
-	GreekFootHashTable()
-)
-source = fragments[0]
-target = fragments[-3]
-print(source, target)
-dist, prev = dijkstra.dijkstra(
-	data=fragments,
-	source = source,
-	target = target,
-	weights = {"gap": 0.75, "onsets": 0.25}
-)
-for x in prev:
-	print(x)
+# fragments = rolling_hash_search(
+# 	filepath,
+# 	0,
+# 	GreekFootHashTable()
+# )
+# source = fragments[0]
+# target = fragments[-3]
+# print(source, target)
+# dist, prev = dijkstra.dijkstra(
+# 	data=fragments,
+# 	source = source,
+# 	target = target,
+# 	weights = {"gap": 0.75, "onsets": 0.25}
+# )
+# for x in prev:
+# 	print(x)
 # for x in prev:
 # 	print(x)
 # path = dijkstra.reconstruct_standard_path(
