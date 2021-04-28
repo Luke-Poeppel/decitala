a32cff746b7fb142a0e990eb4c3776b6b0d37085
Luke Poeppel <luke.poeppel@gmail.com>
2021-04-28 14:10:31 -0400

    Fixed all tests for search.

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,34 +6,19 @@ from decitala.fragment import GreekFoot
 here = os.path.abspath(os.path.dirname(__file__))
 filepath = os.path.dirname(here) + "/tests/static/Shuffled_Transcription_1.xml"
 
-required_tags = {
-	"onset_range",
-	"frag_type",
-	"fragment",
-	"mod",
-	"factor",
-	"difference",
-	"pitch_content",
-	"is_spanned_by_slur"
-}
-
-# def test_rolling_hash_search():
-# 	res = search.rolling_hash_search(
-# 		filepath = filepath,
-# 		part_num = 0,
-# 		table = hash_table.FragmentHashTable(
-# 			datasets=["greek_foot"]
-# 		)
-# 	)
-# 	print(res)
-
-# print(test_rolling_hash_search())
+def test_rolling_hash_search():
+	res = search.rolling_hash_search(
+		filepath = filepath,
+		part_num = 0,
+		table = hash_table.GreekFootHashTable()
+	)
+	assert len(res) == 18
 
 def test_shuffled_I_path_with_slur_constraint():
 	path = search.path_finder(
 		filepath = filepath,
 		part_num=0,
-		frag_type="greek_foot",
+		table=hash_table.GreekFootHashTable(),
 		slur_constraint=True
 	)
 	fragments = [x["fragment"] for x in path]
