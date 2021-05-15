89985923dcc8b84cbf0b8eede7ddfd07b9b585c8
Luke Poeppel <luke.poeppel@gmail.com>
2021-05-15 17:55:36 -0400

    Revamped the doctest_runner file to allow argv module input.

--- tests/doctest_runner.py
+++ tests/doctest_runner.py
@@ -3,29 +3,42 @@ There are a number of doctests in the source, but they cannot be run in the
 standard way as the files have relative imports. As such, we may run them here. 
 """
 import doctest
+import sys
+import importlib
 
-from decitala.utils import get_logger
-logger = get_logger(__file__)
+from decitala import (
+	database,
+	fragment,
+	utils,
+	trees,
+	search,
+	hash_table
+)
+from decitala.path_finding import (
+	dijkstra,
+	floyd_warshall,
+	path_finding_utils,
+	pofp
+)
+ALL_MODULES = [database, fragment, utils, trees, search, hash_table, dijkstra, floyd_warshall, path_finding_utils]#, pofp]
 
-def doctest_runner():
-	logger.info("Running all doctests...")
-	from decitala import (
-		database,
-		fragment,
-		utils,
-		trees,
-		search,
-		hash_table
-	)
-	all_modules = [database, utils, hash_table, trees, fragment, search]
-	for module in all_modules:
+logger = utils.get_logger(__file__)
+
+def doctest_runner(module=None):
+	if module:
 		logger.info("Testing: {}".format(module))
-		fail_count, test_count = doctest.testmod(module)
-		if fail_count != 0:
-			logger.info("{} has doctest failures!".format(module))
-			return False
-		else:
-			continue
+		logger.info(doctest.testmod(module))
+	else:
+		logger.info("Running all doctests...")
+		for this_module in ALL_MODULES:
+			logger.info("Testing: {}".format(this_module))
+			logger.info(doctest.testmod(this_module))
 
 if __name__ == "__main__":
-	doctest_runner()
+	try:
+		arg_in = sys.argv[1]
+		if arg_in:
+			mod = globals()["fragment"]
+			doctest_runner(module=mod)
+	except IndexError:
+		doctest_runner()
