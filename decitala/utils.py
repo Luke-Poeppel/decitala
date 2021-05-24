d36bed9a0775fa113025b56b99933fff0ccabaf0
Luke Poeppel <luke.poeppel@gmail.com>
2021-05-23 21:56:40 -0400

    Bugfix for rolling_SRR.

--- decitala/utils.py
+++ decitala/utils.py
@@ -823,7 +823,7 @@ def rolling_SRR(
 	all_windows_ql = []
 	for window in all_windows:
 		if ignore_tuplets:
-			if any(x.duration.tuplets for x in window):
+			if any(x[0].duration.tuplets for x in window):
 				continue
 		qls = [x[0].quarterLength for x in window]
 		all_windows_ql.append(qls)
