d6e1ce34a0acbd525382807243ad44814bbf9e0a
Luke Poeppel <luke.poeppel@gmail.com>
2021-06-27 19:26:16 -0400

    Updated tests for morris reduction.

--- tests/test_contour.py
+++ tests/test_contour.py
@@ -7,9 +7,22 @@ def test_doctests():
 	assert doctest.testmod(contour, raise_on_error=True)
 
 # Examples from the Schultz article.
-def test_prime_contour():
+def test_immediately_flagged_prime_contours_morris():
+	# See page 97
 	contour_1 = [2, 4, 1, 5, 0, 6, 3]
 	contour_2 = [2, 1, 3, 0]
 
-	assert np.array_equal(contour.contour_to_prime_contour(contour_1), np.array([2, 4, 1, 5, 0, 6, 3]))
-	assert np.array_equal(contour.contour_to_prime_contour(contour_2), np.array([2, 1, 3, 0]))
+	prime_contour_1 = contour.contour_to_prime_contour(contour_1, include_depth=True)
+	expected_prime_contour_1 = [2, 4, 1, 5, 0, 6, 3]
+	assert list(prime_contour_1[0]) == expected_prime_contour_1
+	assert prime_contour_1[1] == 0
+	
+	prime_contour_2 = list(contour.contour_to_prime_contour(contour_2))
+	expected_prime_contour_2 = [2, 1, 3, 0]
+	assert prime_contour_2 == expected_prime_contour_2
+
+def test_contours_morris():
+	contour_c = [1, 3, 1, 2, 0, 1, 4]
+	calculated = contour.contour_to_prime_contour(contour=contour_c, include_depth=3)
+	assert list(calculated[0]) == [1, 0, 2]
+	assert calculated[1] == 3
