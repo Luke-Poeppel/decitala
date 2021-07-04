d55b46cd4f50667d1cbae4f625c0d8be48ba6f3d
Luke Poeppel <luke.poeppel@gmail.com>
2021-07-04 14:21:45 -0400

    Wrote composition versions of the current functions.

--- decitala/extra/hyperparameters.py
+++ decitala/extra/hyperparameters.py
@@ -24,8 +24,41 @@ from decitala.path_finding import (
 	path_finding_utils,
 )
 
+sept_haikai_path = "/Users/lukepoeppel/Messiaen/Encodings/Sept_Haikai/1_Introduction.xml"
+liturgie_path = "/Users/lukepoeppel/Messiaen/Encodings/Messiaen_Qt/Messiaen_I_Liturgie/Messiaen_I_Liturgie_de_cristal_CORRECTED.xml" # noqa
+livre_dorgue_path = "/Users/lukepoeppel/Messiaen/Encodings/Livre_d\'Orgue/V_Piece_En_Trio_Corrected.xml" # noqa
+
+compositions = {
+	"sept_haikai_0": {
+		"filepath": sept_haikai_path,
+		"part_num": 0,
+		"annotation": utils.loader("/Users/lukepoeppel/decitala/databases/analyses/sept_haïkaï_0_analysis.json") # noqa
+	},
+	"sept_haikai_1": {
+		"filepath": sept_haikai_path,
+		"part_num": 1,
+		"annotation": utils.loader("/Users/lukepoeppel/decitala/databases/analyses/sept_haïkaï_1_analysis.json") # noqa
+	},
+	"liturgie": {
+		"filepath": liturgie_path,
+		"part_num": 3,
+		"annotation": utils.loader("/Users/lukepoeppel/decitala/databases/analyses/liturgie_3-4_analysis.json") # noqa
+	},
+	"livre_dorgue_0": {
+		"filepath": livre_dorgue_path,
+		"part_num": 0,
+		"annotation": utils.loader("/Users/lukepoeppel/decitala/databases/analyses/livre_dorgue_0_analysis.json") # noqa
+	},
+	"livre_dorgue_1": {
+		"filepath": livre_dorgue_path,
+		"part_num": 1,
+		"annotation": utils.loader("/Users/lukepoeppel/decitala/databases/analyses/livre_dorgue_1_analysis.json") # noqa
+	},
+}
+
 logger = utils.get_logger(name=__file__, print_to_console=True)
-ALLOW_SUBDIVISION = False
+ALLOW_TRANSCRIPTION_SUBDIVISION = False
+ALLOW_COMPOSITION_SUBDIVISION = True
 ENFORCE_EARLIEST_START = True
 
 def test_single_transcription(transcription, point, verbose=False, show_fragments=False):
@@ -39,7 +72,7 @@ def test_single_transcription(transcription, point, verbose=False, show_fragment
 		filepath=transcription.filepath,
 		part_num=0,
 		table=hash_table.GreekFootHashTable(),
-		allow_subdivision=ALLOW_SUBDIVISION,
+		allow_subdivision=ALLOW_TRANSCRIPTION_SUBDIVISION,
 		cost_function_class=cost,
 		split_dict=path_finding_utils.default_split_dict(),
 		enforce_earliest_start=ENFORCE_EARLIEST_START,
@@ -57,7 +90,38 @@ def test_single_transcription(transcription, point, verbose=False, show_fragment
 		return_list=True
 	)
 
-def test_all_on_single_point(point):
+def test_single_work(work, point, verbose=False, show_fragments=False):
+	"""Calculates accuracy of a point for a single part in a composition. Uses DHT."""
+	cost = path_finding_utils.CostFunction3D(
+		gap_weight=point[0],
+		onset_weight=point[1],
+		articulation_weight=point[2]
+	)
+	path = search.path_finder(
+		filepath=compositions[work]["filepath"],
+		part_num=compositions[work]["part_num"],
+		table=hash_table.DecitalaHashTable(),
+		allow_subdivision=ALLOW_COMPOSITION_SUBDIVISION,
+		cost_function_class=cost,
+		split_dict=path_finding_utils.default_split_dict(),
+		enforce_earliest_start=ENFORCE_EARLIEST_START,
+		verbose=verbose
+	)
+	if show_fragments:
+		for x in path:
+			logger.info(x.fragment, x.onset_range, x.is_spanned_by_slur, x.pitch_content, x.mod_hierarchy_val) # noqa
+		print()
+
+	return path_finding_utils.check_accuracy(
+		training_data=compositions[work]["annotation"],
+		calculated_data=path,
+		mode="Compositions",
+		return_list=True
+	)
+
+# print(test_single_work("livre_dorgue_0", point=[0.8, 0.1, 0.1]))
+
+def test_all_transcriptions_on_single_point(point):
 	"""Calculates accuracy for single input point."""
 	total = 0
 	correct = 0
@@ -76,9 +140,29 @@ def test_all_on_single_point(point):
 
 	return (correct, total, correct / total)
 
-# print(test_all_on_single_point([0.8, 0.1, 0.1]))
+# print(test_all_transcriptions_on_single_point([0.8, 0.1, 0.1]))
 
-def test_all_points(resolution=0.1):
+def test_all_works_on_single_point(point):
+	"""Calculates accuracy for single input point."""
+	total = 0
+	correct = 0
+	for work, data in compositions.items():
+		logger.info(work)
+		res = test_single_work(
+			work=work,
+			point=point,
+			show_fragments=False,
+			verbose=False
+		)
+		logger.info(res)
+		correct += res[0]
+		total += res[1]
+
+	return (correct, total, correct / total)
+
+# print(test_all_works_on_single_point([0.8, 0.1, 0.1]))
+
+def test_all_points_transcriptions(resolution=0.1):
 	date = datetime.today().strftime("%m-%d-%Y")
 	ALL_RES = dict()
 	for transcription in get_all_transcriptions():
@@ -96,6 +180,26 @@ def test_all_points(resolution=0.1):
 	with open(filepath, "w") as fp:
 		json.dump(obj=ALL_RES, fp=fp, ensure_ascii=False, indent=4)
 	return
+
+def test_all_points_works(resolution=0.1):
+	date = datetime.today().strftime("%m-%d-%Y")
+	ALL_RES = dict()
+	for work in compositions:
+		results = dict()
+		logger.info(work)
+		for point in path_finding_utils.make_3D_grid(resolution=resolution):
+			result = test_single_work(work, point=point)
+			logger.info(result)
+			results[str(point)] = str(result)
+
+		ALL_RES[work] = results
+
+	filepath = f"/Users/lukepoeppel/decitala/decitala/extra/{date}_composition_hyperparameters_{resolution}_greek_foot_4.json" # noqa
+	with open(filepath, "w") as fp:
+		json.dump(obj=ALL_RES, fp=fp, ensure_ascii=False, indent=4)
+	return
+
+# print(test_all_points_works())
 
 ####################################################################################################
 # Plotting results
