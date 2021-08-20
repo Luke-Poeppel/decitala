b339d8dcac569603d892418889df5de5d89dbe90
Luke Poeppel <luke.poeppel@gmail.com>
2021-08-20 17:22:47 -0400

    Simplified animation.

--- decitala/extra/dijkstra_gif.py
+++ decitala/extra/dijkstra_gif.py
@@ -11,7 +11,6 @@ import matplotlib as mpl
 import subprocess
 import itertools
 import random
-import shutil
 import os
 import natsort
 
@@ -37,6 +36,13 @@ def _stupid_flatten(data):
 			out.append(val)
 	return out
 
+def _gif_from_source(fps, rate=None, save_path=None):
+	if rate is not None:
+		run_elems = ["convert", "-delay", f"{rate}", fps, "-loop", "0", save_path]
+	else:
+		run_elems = ["convert", fps, save_path]
+	subprocess.run(_stupid_flatten(run_elems))
+
 def _plot_base(all_data, title=None):
 	"""
 	Plots the base data.
@@ -53,18 +59,6 @@ def _plot_base(all_data, title=None):
 	if title:
 		plt.title(title, fontname="Times", fontsize=14)
 
-def _dijkstra_gif_pair(i, data, pair, title=None, save_path=None):
-	plt.text(0.05, pair[1].onset_range[1], f"Pair {i}...", fontname="Times")
-	_plot_base(data)
-
-	plt.scatter(pair[0].onset_range[0], pair[0].onset_range[1], marker="d", s=25, color="g")
-	plt.scatter(pair[1].onset_range[0], pair[1].onset_range[1], marker="d", s=25, color="g")
-
-	curr_path = save_path + f"/f{(5 * i) + 1}.png"
-	plt.savefig(curr_path, dpi=150)
-	for j in range(4):
-		shutil.copyfile(curr_path, save_path + f"/f{(5 * i) + 2 + j}.png")
-
 def _dijkstra_gif_animate_path(i, all_data, pair, cost_function, title=None, save_path=None):
 	path = dijkstra.naive_dijkstra_path(
 		data=all_data,
@@ -72,9 +66,10 @@ def _dijkstra_gif_animate_path(i, all_data, pair, cost_function, title=None, sav
 		target=pair[1]
 	)
 	path = sorted([x for x in all_data if x.id_ in path], key=lambda x: x.onset_range[0])
+	net_cost = path_finding_utils.net_cost(path)
 	j = 0
 	while j < len(path):
-		plt.text(0.05, pair[1].onset_range[1], f"Pair {i}...", fontname="Times")
+		plt.text(0.05, pair[1].onset_range[1], f"Pair {i}: {net_cost}", fontname="Times")
 		_plot_base(all_data)
 		plt.scatter(pair[0].onset_range[0], pair[0].onset_range[1], marker="d", s=25, color="g")
 		plt.scatter(pair[1].onset_range[0], pair[1].onset_range[1], marker="d", s=25, color="g")
@@ -83,23 +78,13 @@ def _dijkstra_gif_animate_path(i, all_data, pair, cost_function, title=None, sav
 		ys = [x.onset_range[1] for x in path[0:j + 1]]
 		plt.plot(xs, ys, "--o", color="r", markersize=3, linewidth=1)
 
+		if title:
+			plt.title(title, fontname="Times", fontsize=12)
+
 		out = os.path.join(save_path, f"animate_path_{i}_elem{j}.png")
 		plt.savefig(out, dpi=150)
 		plt.clf()
 		j += 1
-
-def _gif_from_source(fps, rate, save_path, logger):
-	run_elems = [
-		"convert",
-		"-delay",
-		f"{rate}",
-		fps,
-		"-loop",
-		"0",
-		save_path,
-	]
-	logger.info("Creating component GIF...")
-	subprocess.run(_stupid_flatten(run_elems))
 
 def dijkstra_gif(
 		filepath,
@@ -114,7 +99,6 @@ def dijkstra_gif(
 		enforce_earliest_start=False,
 		title=None,
 		rate=20,
-		show=False,
 		save_path=None
 	):
 	"""
@@ -127,7 +111,7 @@ def dijkstra_gif(
 							the GIF.
 	"""
 	if not(save_path):
-		raise Exception("No path provided.")
+		raise Exception("No directory path provided.")
 
 	logger = get_logger(name=__file__)
 
@@ -142,26 +126,6 @@ def dijkstra_gif(
 		allow_subdivision=allow_subdivision,
 		allow_contiguous_summation=allow_contiguous_summation
 	)
-	xs = [x.onset_range[0] for x in all_data]
-	ys = [x.onset_range[1] for x in all_data]
-	plt.scatter(xs, ys, s=5, color="k")
-
-	plt.xticks(fontname="Times")
-	plt.xlabel("Offset Start", fontname="Times", fontsize=12)
-	plt.yticks(fontname="Times")
-	plt.ylabel("Offset End", fontname="Times", fontsize=12)
-
-	if title:
-		plt.title(title, fontname="Times", fontsize=14)
-	plt.tight_layout()
-
-	logger.info("Saving base image...")
-	base_path = save_path + "/f1.png"
-	plt.savefig(base_path, dpi=150)
-	for i in range(2, 6):
-		shutil.copyfile(base_path, save_path + f"/f{i}.png")
-
-	plt.clf()
 	################################################################################################
 	# Plot each pair
 	sources, sinks = path_finding_utils.sources_and_sinks(
@@ -172,15 +136,14 @@ def dijkstra_gif(
 	random.shuffle(pairs)
 
 	# REMOVE –– JUST FOR TESTING
-	pairs = pairs[0:2]
+	pairs = pairs[0:4]
 
 	for i, pair in enumerate(pairs, start=1):
 		logger.info(f"Writing pair {i}")
-		_dijkstra_gif_pair(i, all_data, pair, title, save_path)
 		plt.clf()
 		_dijkstra_gif_animate_path(i, all_data, pair, cost_function_class, title=title, save_path=save_path) # noqa
 
-	################################################################################################
+	# ################################################################################################
 	# Making the component GIFS:
 	for i in range(1, len(pairs) + 1):
 		curr_path_source = []
@@ -188,18 +151,19 @@ def dijkstra_gif(
 			if fp.startswith(f"animate_path_{i}"):
 				curr_path_source.append(os.path.join(save_path, fp))
 		curr_path_source = natsort.natsorted(curr_path_source)
-		curr_gif_path = os.path.join(save_path, f"path_{i}_gif.gif")
-		_gif_from_source(fps=curr_path_source, rate=5, save_path=curr_gif_path, logger=logger)
-
-	# gif_filepath = os.path.join(save_path, "final.gif")
-	# file_order = [os.path.join(save_path, f"f{i}.png") for i in range(1, len(os.listdir(save_path)) + 1)] # noqa
+		curr_gif_path = os.path.join(save_path, f"path_{i}.gif")
+		_gif_from_source(fps=curr_path_source, rate=5, save_path=curr_gif_path)
 
 	################################################################################################
 	# Combining the component GIFS.
+	final_filepath = os.path.join(save_path, "final.gif")
+	curr_path_source_gifs = natsort.natsorted(
+		[os.path.join(save_path, this_file) for this_file in os.listdir(save_path) if this_file.startswith("path_")] # noqa
+	)
+	components = [curr_path_source_gifs]
+	_gif_from_source(fps=components, save_path=final_filepath)
 
 	################################################################################################
-	if show:
-		plt.show()
 
 def test_dijkstra_gif():
 	fp = "/Users/lukepoeppel/Messiaen/Encodings/Sept_Haikai/1_Introduction.xml"
@@ -211,7 +175,6 @@ def test_dijkstra_gif():
 		allow_subdivision=True,
 		title="Iterated Dijkstra on Sept Haïkaï (Bois)",
 		save_path=f"/Users/lukepoeppel/decitala/tests/dijkstra_gif_tests/dijkstra_{uuid.uuid4().hex}",
-		show=False
 	)
 
 # print(test_dijkstra_gif())
