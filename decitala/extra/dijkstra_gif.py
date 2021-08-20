ff4b554df4946aebf80acd62ab0edb9d91580d79
Luke Poeppel <luke.poeppel@gmail.com>
2021-08-20 14:10:18 -0400

    Add animated path.

--- decitala/extra/dijkstra_gif.py
+++ decitala/extra/dijkstra_gif.py
@@ -13,18 +13,36 @@ import itertools
 import random
 import shutil
 import os
+import natsort
 
 from decitala import search
 from decitala.path_finding import (
-	path_finding_utils
+	path_finding_utils,
+	dijkstra
 )
 from decitala.utils import get_logger
+from decitala.hash_table import DecitalaHashTable
 
 mpl.style.use("bmh")
 
-def _plot_base(data, title=None):
-	xs = [x.onset_range[0] for x in data]
-	ys = [x.onset_range[1] for x in data]
+def _stupid_flatten(data):
+	"""
+	Little utility function used in the GIF maker from imagemagick.
+	"""
+	out = []
+	for val in data:
+		if isinstance(val, list):
+			out.extend(_stupid_flatten(val))
+		else:
+			out.append(val)
+	return out
+
+def _plot_base(all_data, title=None):
+	"""
+	Plots the base data.
+	"""
+	xs = [x.onset_range[0] for x in all_data]
+	ys = [x.onset_range[1] for x in all_data]
 	plt.scatter(xs, ys, s=5, color="k")
 
 	plt.xticks(fontname="Times")
@@ -34,15 +52,6 @@ def _plot_base(data, title=None):
 
 	if title:
 		plt.title(title, fontname="Times", fontsize=14)
-
-def _stupid_flatten(data):
-	out = []
-	for val in data:
-		if isinstance(val, list):
-			out.extend(_stupid_flatten(val))
-		else:
-			out.append(val)
-	return out
 
 def _dijkstra_gif_pair(i, data, pair, title=None, save_path=None):
 	plt.text(0.05, pair[1].onset_range[1], f"Pair {i}...", fontname="Times")
@@ -56,20 +65,41 @@ def _dijkstra_gif_pair(i, data, pair, title=None, save_path=None):
 	for j in range(4):
 		shutil.copyfile(curr_path, save_path + f"/f{(5 * i) + 2 + j}.png")
 
-def _dijkstra_gif_animate_path(i, data, pair, cost_function, title=None, save_path=None):
-	plt.text(0.05, pair[1].onset_range[1], f"Pair {i}...", fontname="Times")
+def _dijkstra_gif_animate_path(i, all_data, pair, cost_function, title=None, save_path=None):
+	path = dijkstra.naive_dijkstra_path(
+		data=all_data,
+		source=pair[0],
+		target=pair[1]
+	)
+	path = sorted([x for x in all_data if x.id_ in path], key=lambda x: x.onset_range[0])
+	j = 0
+	while j < len(path):
+		plt.text(0.05, pair[1].onset_range[1], f"Pair {i}...", fontname="Times")
+		_plot_base(all_data)
+		plt.scatter(pair[0].onset_range[0], pair[0].onset_range[1], marker="d", s=25, color="g")
+		plt.scatter(pair[1].onset_range[0], pair[1].onset_range[1], marker="d", s=25, color="g")
 
-	_plot_base(data)
-	plt.scatter(pair[0].onset_range[0], pair[0].onset_range[1], marker="d", s=25, color="g")
-	plt.scatter(pair[1].onset_range[0], pair[1].onset_range[1], marker="d", s=25, color="g")
+		xs = [x.onset_range[0] for x in path[0:j + 1]]
+		ys = [x.onset_range[1] for x in path[0:j + 1]]
+		plt.plot(xs, ys, "--o", color="r", markersize=3, linewidth=1)
 
-	# for elem of path:
-	# 	for i in range()
+		out = os.path.join(save_path, f"animate_path_{i}_elem{j}.png")
+		plt.savefig(out, dpi=150)
+		plt.clf()
+		j += 1
 
-	# if path:
-	# 	xs = [x.onset_range[0] for x in path]
-	# 	ys = [x.onset_range[1] for x in path]
-	# 	plt.plot(xs, ys, "--o", color="r", markersize=3, linewidth=1, label="Extracted Path")
+def _gif_from_source(fps, rate, save_path, logger):
+	run_elems = [
+		"convert",
+		"-delay",
+		f"{rate}",
+		fps,
+		"-loop",
+		"0",
+		save_path,
+	]
+	logger.info("Creating component GIF...")
+	subprocess.run(_stupid_flatten(run_elems))
 
 def dijkstra_gif(
 		filepath,
@@ -141,46 +171,47 @@ def dijkstra_gif(
 	pairs = list(itertools.product(sources, sinks))
 	random.shuffle(pairs)
 
-	for i, pair in enumerate(pairs[0:5], start=1):
+	# REMOVE –– JUST FOR TESTING
+	pairs = pairs[0:2]
+
+	for i, pair in enumerate(pairs, start=1):
 		logger.info(f"Writing pair {i}")
 		_dijkstra_gif_pair(i, all_data, pair, title, save_path)
 		plt.clf()
-		# _dijkstra_gif_animate_path(i, all_data, pair, cost_function_class, title=title, save_path=save_path) # noqa
-		# plt.clf()
+		_dijkstra_gif_animate_path(i, all_data, pair, cost_function_class, title=title, save_path=save_path) # noqa
 
 	################################################################################################
-	# Making the GIF:
-	gif_filepath = os.path.join(save_path, "final.gif")
-	file_order = [os.path.join(save_path, f"f{i}.png") for i in range(1, len(os.listdir(save_path)) + 1)] # noqa
-	run_elems = [
-		"convert",
-		"-delay",
-		str(rate),
-		file_order,
-		"-loop",
-		"0",
-		gif_filepath,
-	]
-	logger.info("Creating GIF...")
-	subprocess.run(_stupid_flatten(run_elems))
+	# Making the component GIFS:
+	for i in range(1, len(pairs) + 1):
+		curr_path_source = []
+		for fp in os.listdir(save_path):
+			if fp.startswith(f"animate_path_{i}"):
+				curr_path_source.append(os.path.join(save_path, fp))
+		curr_path_source = natsort.natsorted(curr_path_source)
+		curr_gif_path = os.path.join(save_path, f"path_{i}_gif.gif")
+		_gif_from_source(fps=curr_path_source, rate=5, save_path=curr_gif_path, logger=logger)
+
+	# gif_filepath = os.path.join(save_path, "final.gif")
+	# file_order = [os.path.join(save_path, f"f{i}.png") for i in range(1, len(os.listdir(save_path)) + 1)] # noqa
+
+	################################################################################################
+	# Combining the component GIFS.
 
 	################################################################################################
 	if show:
 		plt.show()
 
-
-# def test_dijkstra_gif():
-# 	# fp = os.path.dirname(here) + "/tests/static/Shuffled_Transcription_1.xml"
-# 	fp = "/Users/lukepoeppel/Messiaen/Encodings/Sept_Haikai/1_Introduction.xml"
-# 	import uuid
-# 	vis.dijkstra_gif(
-# 		filepath=fp,
-# 		part_num=0,
-# 		table=DecitalaHashTable(),
-# 		allow_subdivision=True,
-# 		title="Iterated Dijkstra on Sept Haïkaï (Bois)",
-# 		save_path=f"/Users/lukepoeppel/decitala/tests/dijkstra_gif_tests/dijkstra_{uuid.uuid4().hex}",
-# 		show=False
-# 	)
+def test_dijkstra_gif():
+	fp = "/Users/lukepoeppel/Messiaen/Encodings/Sept_Haikai/1_Introduction.xml"
+	import uuid
+	dijkstra_gif(
+		filepath=fp,
+		part_num=0,
+		table=DecitalaHashTable(),
+		allow_subdivision=True,
+		title="Iterated Dijkstra on Sept Haïkaï (Bois)",
+		save_path=f"/Users/lukepoeppel/decitala/tests/dijkstra_gif_tests/dijkstra_{uuid.uuid4().hex}",
+		show=False
+	)
 
 # print(test_dijkstra_gif())
